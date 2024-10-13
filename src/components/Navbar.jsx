6341b4d4c823bacda84a4b914492f2c5e138d15a
Meredith Butterworth <meredithbutterworth@gmail.com>
2024-10-13 12:01:45 -0400

    padding/margins in navbar and overline decoration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,9 +14,9 @@ function Navbar ({token, setToken}) {
     return (
         <div id="navbar" className="nav-links">
             {/* <h1 className="header">Lorem's Bodega</h1> */}
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/products">Catalog</NavLink>
-            <NavLink to="/SignUp">Login</NavLink>
+            <NavLink className="navlink1" to="/">Home</NavLink>
+            <NavLink className="navlink2" to="/products">Catalog</NavLink>
+            <NavLink className="navlink3" to="/SignUp">Login</NavLink>
         </div>
     );
 }
